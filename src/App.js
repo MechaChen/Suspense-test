971a4aa27ac7925f8a80cfbae148a04e690c9020
Benson Chen <tumlivein@gmail.com>
2023-08-09 05:24:11 +0000

    feat: add lazy loading and suspense

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import './App.css';
+import { useState, Suspense, lazy } from 'react';
 
-function App() {
+import Loading from './Loading';
+
+const MarkdownPreview = lazy(() => delayForDemo(import('./MarkdownPreview.js')));
+
+
+async function delayForDemo (promise) {
+  const delay = new Promise((resolve) => setTimeout(resolve, 2000))
+  await delay;
+  return promise;
+}
+
+const MarkdownEditor = function MarkdownEditor() {
+  const [showPreview, setShowPreview] = useState(false);
+  const [markdown, setMarkdown] = useState('Hello, **world**!');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+      <>
+          <textarea value={markdown} onChange={e => setMarkdown(e.target.value)} />
+          <label>
+              <input type="checkbox" checked={showPreview} onChange={e => setShowPreview(e.target.checked)} />
+              Show preview
+          </label>
+          <hr />
+          {showPreview && (
+              <Suspense fallback={<Loading />}>
+                  <h2>Preview</h2>
+                  <MarkdownPreview markdown={markdown} />
+              </Suspense>
+          )}
+      </>
   );
 }
 
-export default App;
+export default MarkdownEditor;
+
